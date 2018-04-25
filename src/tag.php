60090c6d86973ac9aaf57c765ff1947dab72e816
Will Roscoe <1784940+willroscoe@users.noreply.github.com>
2018-04-25 11:25:36 +0100

    Added new templates for news and tags

---
+++ src/tag.php
@@ -0,0 +1,64 @@
+<?php
+/**
+ * The template for displaying Tag pages
+ *
+ * Used to display archive-type pages for posts in a tag.
+ *
+ * @link https://codex.wordpress.org/Template_Hierarchy
+ *
+ * @package Mattering_Press
+ * @subpackage Mattering_Press
+ * @since Mattering Press 1.0
+ */
+
+get_header(); ?>
+
+	<div id="primary" class="content-area">
+		<main id="content" class="site-content" role="main">
+
+			<?php if ( have_posts() ) : ?>
+
+			<header class="archive-header">
+				<h1 class="archive-title"><?php printf( __( 'Tag Archives: %s', 'matteringpress' ), single_tag_title( '', false ) ); ?></h1>
+
+				<?php
+					// Show an optional term description.
+					$term_description = term_description();
+					if ( ! empty( $term_description ) ) :
+						printf( '<div class="taxonomy-description">%s</div>', $term_description );
+					endif;
+				?>
+			</header><!-- .archive-header -->
+
+			<?php
+					// Start the Loop.
+					while ( have_posts() ) : the_post();
+
+						/*
+						 * Include the post format-specific template for the content. If you want to
+						 * use this in a child theme, then include a file called called content-___.php
+						 * (where ___ is the post format) and that will be used instead.
+						 */
+						get_template_part( 'content', get_post_format() );
+
+					endwhile;
+					// Previous/next page navigation.
+					the_posts_pagination( array(
+						'prev_text'          => __( 'Previous page', 'matteringpress' ),
+						'next_text'          => __( 'Next page', 'matteringpress' ),
+						'before_page_number' => '<span class="meta-nav screen-reader-text">' . __( 'Page', 'matteringpress' ) . ' </span>',
+					) );
+
+				else :
+					// If no content, include the "No posts found" template.
+					get_template_part( 'content', 'none' );
+
+				endif;
+			?>
+		</main><!-- #content -->
+	</div><!-- #primary -->
+
+<?php
+
+get_sidebar();
+get_footer();
---
+++ src/template-parts/news.php
@@ -0,0 +1,41 @@
+<?php
+/**
+ * The template part for displaying content
+ *
+ * @package Mattering_Press
+ * @subpackage Mattering_Press
+ * @since Mattering Press 1.0
+ */
+?>
+<article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
+	<header class="entry-header">
+		<?php if ( is_sticky() && is_home() && ! is_paged() ) : ?>
+			<span class="sticky-post"><?php _e( 'Featured', 'matteringpress' ); ?></span>
+		<?php endif; ?>
+
+		<?php the_title( sprintf( '<h2 class="entry-title"><a href="%s" rel="bookmark">', esc_url( get_permalink() ) ), '</a></h2>' ); ?>
+	</header><!-- .entry-header -->
+
+	<?php if (get_post_type() == 'post') { ?>
+		<p class="post-word-count"><?php echo matteringpress_post_word_count(3); ?></p>
+	<?php } ?>
+	<div class="entry-main-content">
+	<?php matteringpress_post_thumbnail(); ?>
+	
+	<?php the_excerpt(); ?>
+	</div>
+
+	<footer class="entry-footer">
+		<?php
+			edit_post_link(
+				sprintf(
+					/* translators: %s: Name of current post */
+					__( 'Edit<span class="screen-reader-text"> "%s"</span>', 'matteringpress' ),
+					get_the_title()
+				),
+				'<span class="edit-link">',
+				'</span>'
+			);
+		?>
+	</footer><!-- .entry-footer -->
+</article><!-- #post-## -->
